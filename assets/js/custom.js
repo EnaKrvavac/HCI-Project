95cae6d20f6a54fcb3512fe1a846f8b6c49fb67f
EnaKrvavac <ena.krvavac@stu.ibu.edu.ba>
2021-06-14 16:14:12 +0200

    First commit

---
+++ assets/js/custom.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+
+  $("main#spapp > section").height($(document).height() - 60);
+
+  var app = $.spapp({pageNotFound : 'error_404'}); // initialize
+
+
+  app.route({
+    view: 'home',
+    load: 'index.html'
+  });
+
+  app.route({
+    view: 'schedule',
+    load: 'schedule.html'
+  });
+
+  app.route({
+    view: 'contact',
+    load: 'contact.html'
+  });
+
+  app.route({
+    view: 'about',
+    load: 'about.html'
+
+  });
+
+  app.run();
+
+});
